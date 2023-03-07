cff0a0de33e75acc4dcbd1e205d0a1d1ec434673
Димо Димов <dpdimov18@codingburgas.bg>
2023-03-07 16:46:49 +0200

    Add project files.

---
+++ Kondio/Data/UsersContext.cs
@@ -0,0 +1,15 @@
+﻿using Kondio.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kondio.Data
+{
+    public class UsersContext : DbContext
+    {
+        public UsersContext(DbContextOptions<UsersContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
